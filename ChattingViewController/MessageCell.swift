928ff252ffc0705cec413b34a8df7e130696daa9
Itallo Rossi Lucas <itallorossi@Itallos-MacBook-Pro.local>
2017-08-04 00:42:28 -0300

    Start building the message cell.

---
+++ ChattingViewController/MessageCell.swift
@@ -0,0 +1,51 @@
+//
+//  MessageCell.swift
+//  ChattingViewController
+//
+//  Created by Itallo Rossi Lucas on 04/08/17.
+//  Copyright © 2017 kallahir. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UICollectionViewCell {
+    
+    let textView: UITextView = {
+        let textView = UITextView()
+        textView.text = "SAMPLE"
+        textView.font = UIFont.systemFont(ofSize: 16)
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.backgroundColor = UIColor.clear
+        return textView
+    }()
+    
+    let bubbleView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.blue
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        addSubview(bubbleView)
+        addSubview(textView)
+        
+        bubbleView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        bubbleView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        bubbleView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        bubbleView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+        
+        textView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
+        textView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        textView.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        textView.heightAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
--- ChattingViewController/ViewController.swift
+++ ChattingViewController/ViewController.swift
@@ -8,8 +8,10 @@
 
 import UIKit
 
-class ViewController: UICollectionViewController {
+class ViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
+    var messages: [String] = ["message01","message02","message03"]
+    
     let inputTextField: UITextField = {
         let textField = UITextField()
         textField.placeholder = "Enter message..."
@@ -20,6 +22,9 @@ class ViewController: UICollectionViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.backgroundColor = UIColor.white
+        collectionView?.contentInset = UIEdgeInsets(top: 8, left: 0, bottom: 8, right: 0)
+        collectionView?.alwaysBounceVertical = true
+        collectionView?.register(MessageCell.self, forCellWithReuseIdentifier: "MessageCell")
         navigationItem.title = "Chatting"
         setupInput()
     }
@@ -61,11 +66,33 @@ class ViewController: UICollectionViewController {
         separatorLineView.topAnchor.constraint(equalTo: containerView.topAnchor).isActive = true
         separatorLineView.widthAnchor.constraint(equalTo: containerView.widthAnchor).isActive = true
         separatorLineView.heightAnchor.constraint(equalToConstant: 1).isActive = true
+        
+        print(messages.count)
     }
     
     func send() {
         print(inputTextField.text ?? "")
+        messages.append(inputTextField.text ?? "")
+        collectionView?.reloadData()
         inputTextField.text = ""
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MessageCell", for: indexPath) as! MessageCell
+        
+        let message = messages[indexPath.item]
+        cell.textView.text = message
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = UIScreen.main.bounds.width
+        return CGSize(width: width, height: 80)
     }
 
 }
