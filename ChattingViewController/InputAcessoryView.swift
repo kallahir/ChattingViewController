742113cd9862277cd0850bb6c1853adbe7ddc6cf
Itallo Rossi Lucas <itallorossi.lucas@gmail.com>
2017-08-08 02:06:11 -0300

    Separation of input acessory view in another class, making possible to improve the input text area.

---
+++ ChattingViewController/InputAcessoryView.swift
@@ -0,0 +1,9 @@
+//
+//  InputAcessoryView.swift
+//  ChattingViewController
+//
+//  Created by Itallo Rossi Lucas on 08/08/17.
+//  Copyright © 2017 kallahir. All rights reserved.
+//
+
+import Foundation
